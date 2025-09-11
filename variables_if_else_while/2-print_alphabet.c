f7d8d1e279ebcd93fd2117304e3ec54a62e52a90
D-Agbikossi <d.agbikossi@alustudent.com>
2025-09-11 19:37:17 +0300

    Task 2 completed

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,16 @@
 
 /**
  * main - Print the alphabet in lowercase, followed by a new line.
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
- {
-     char letter;
+{
+	char letter;
 
-     for (letter = 'a'; letter <= 'z'; letter++)
-	 putchar(letter);
-     putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
 
-     return (0);
- }
+	return (0);
+}
