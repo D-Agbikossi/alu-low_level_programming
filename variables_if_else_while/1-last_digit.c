ec9842ca3b89387afbabed94880f44cfc26933e8
D-Agbikossi <d.agbikossi@alustudent.com>
2025-09-11 19:16:26 +0300

    1-last_digit.c made betty compliant

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- * main -  Print the last digit of a randomly generated number
- * 
- * Return: 0
- */ 
+ * main - Print the last digit of a randomly generated number
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -14,14 +14,21 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	last_digit = n % 10;
+
 	if (last_digit > 5)
+	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
 	else if (last_digit == 0)
+	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 
+			last_digit);
+	}
 
 	return (0);
 }
